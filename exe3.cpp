402e48872d3cf7762a1c2bfa07ce769fb407baad
Hadarfro <122720878+Hadarfro@users.noreply.github.com>
2024-05-05 10:23:06 +0300

    fixing makefile

--- exe3.cpp
+++ exe3.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <dlfcn.h>
 #include <vector>
-<<<<<<< HEAD
 #include "exe2.hpp"
-=======
->>>>>>> b8d99493a0b5f092a7a7ba2baba222cfc26ada2e
 
 typedef long double (*pois)();
 long double poisson(int k, long double lambda);
@@ -36,16 +33,8 @@ void printPoisson(){ // Print all the results of poisson's calculations
     std::vector<int> k = {1, 10, 2, 3, 3};
     std::vector<long double> lambda = {2, 2, 2, 3, 100};
 
-<<<<<<< HEAD
     for(decltype(k.size()) i = 0; i < k.size(); i++) {
         long double result = poisson(k[i], lambda[i]);
         std::cout << result << std::endl;
     }
 }
-=======
-    for(int i=0; i<5; i++){
-        long double result = poisson(k[i], lambda[i]);
-        std::cout<<result<<std::endl;
-    }
-}
->>>>>>> b8d99493a0b5f092a7a7ba2baba222cfc26ada2e
