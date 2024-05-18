b3b2a50cc0416990eb7d1980f0aed56e3e234276
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-18 23:40:07 +0300

    Update exe6.cpp

--- exe6.cpp
+++ exe6.cpp
@@ -14,61 +14,7 @@ struct Contact {
     char phoneNumber[100];
 };
 
-// Function to add a contact to a pipe
-void add2PB(int pipefd[1], const Contact& contact, std::vector<Contact>& telephoneBook) {
-    // Close the read end of the pipe since we are writing
-    // close(pipefd[0]);
 
-    // Write the contact data to the pipe
-    ssize_t bytesWritten = write(pipefd[1], &contact, sizeof(contact));
-    if (bytesWritten != sizeof(Contact)) {
-        std::cerr << "Error: Incomplete write to pipe" << std::endl;
-        return;
-    }
-
-    // Add the contact to the telephoneBook vector
-    telephoneBook.push_back(contact);
-
-    // Write the contact to the file
-    std::fstream phonebook("phonebook.txt", std::ios::app); // Open file in append mode
-    if (!phonebook) { // Checking if the file open successfully
-        std::cerr << "Error: Failed to open phonebook.txt for writing" << std::endl;
-        return;
-    }
-    phonebook << contact.name << ", " << contact.phoneNumber << std::endl;
-
-    // Close the file
-    phonebook.close();
-
-    // Close the write end of the pipe after writing
-   // close(pipefd[1]);
-}
-
-// Function to read contacts from a pipe
-void findPhone(int pipefd[2], std::vector<Contact>& telephoneBook) {
-    // Close the write end of the pipe since we are reading
-    close(pipefd[1]);
-
-    // Read contacts from the pipe
-    Contact contact;
-    ssize_t bytesRead;
-    while ((bytesRead = read(pipefd[0], &contact, sizeof(Contact))) > 0) {
-        if (bytesRead != sizeof(Contact)) {
-            std::cerr << "Error: Incomplete read from pipe" << std::endl;
-            return;
-        }
-        telephoneBook.push_back(contact);
-    }
-
-    // Check for read errors
-    if (bytesRead == -1) {
-        std::cerr << "Error: Failed to read from pipe" << std::endl;
-        return;
-    }
-
-    // Close the read end of the pipe after reading
-    close(pipefd[0]);
-}
 // Function to create and populate the phonebook.txt file
 void createPhoneBookFile() {
     std::ofstream phonebook("phonebook.txt"); // Create the file
